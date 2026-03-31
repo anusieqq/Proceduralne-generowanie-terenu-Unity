c0a0968644e76b7ff4c5d57b94a6703a1c1bc6ee
anusieqq <annazapala.2003@gmail.com>
2026-03-31 18:47:18 +0200

    kometarze do kodu

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -2,25 +2,28 @@ using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
+
 [CustomEditor(typeof(MapGenerator))]
 public class MapGeneratorEditor : Editor
 {
-
+    // Rysuje niestandardowy panel w Inspectorze
     public override void OnInspectorGUI()
     {
-        MapGenerator mapGen = (MapGenerator)target;
+        MapGenerator mapGen = (MapGenerator)target; 
 
+        
         if (DrawDefaultInspector())
         {
-            if (mapGen.autoUpdate)
+            if (mapGen.autoUpdate) // je?li autoUpdate w??czone
             {
-                mapGen.GenerateMap();
+                mapGen.GenerateMap(); // generujemy map? automatycznie po zmianie warto?ci
             }
         }
 
+        // Dodaje przycisk "Generate" w Inspectorze
         if (GUILayout.Button("Generate"))
         {
-            mapGen.GenerateMap();
+            mapGen.GenerateMap(); 
         }
     }
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public static class Noise
 {
-
+    // Generuje map? szumów Perlin'a
     public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
+        // Ustawienie generatora liczb pseudolosowych
         System.Random prng = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
         for (int i = 0; i < octaves; i++)
@@ -19,7 +20,7 @@ public static class Noise
 
         if (scale <= 0)
         {
-            scale = 0.0001f;
+            scale = 0.0001f; // unikni?cie dzielenia przez 0
         }
 
         float maxNoiseHeight = float.MinValue;
@@ -28,16 +29,16 @@ public static class Noise
         float halfWidth = mapWidth / 2f;
         float halfHeight = mapHeight / 2f;
 
-
+        // P?tla po wszystkich punktach mapy
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
-
                 float amplitude = 1;
                 float frequency = 1;
                 float noiseHeight = 0;
 
+                // Sumowanie oktaw Perlin Noise
                 for (int i = 0; i < octaves; i++)
                 {
                     float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
@@ -46,22 +47,21 @@ public static class Noise
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
                     noiseHeight += perlinValue * amplitude;
 
-                    amplitude *= persistance;
-                    frequency *= lacunarity;
+                    amplitude *= persistance; // zmniejszanie wp?ywu kolejnej oktawy
+                    frequency *= lacunarity; // zwi?kszanie cz?stotliwo?ci kolejnej oktawy
                 }
 
+                // Zapami?tanie min i max wysoko?ci w mapie
                 if (noiseHeight > maxNoiseHeight)
-                {
                     maxNoiseHeight = noiseHeight;
-                }
                 else if (noiseHeight < minNoiseHeight)
-                {
                     minNoiseHeight = noiseHeight;
-                }
+
                 noiseMap[x, y] = noiseHeight;
             }
         }
 
+        // Normalizacja mapy do zakresu 0-1
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
@@ -72,5 +72,4 @@ public static class Noise
 
         return noiseMap;
     }
-
 }
